b90102a5463c80ef6ba1b6df584fdcf01edcc4f4
loveumearnab2812000 <loveumearnab.2812000@gmail.com>
2023-08-17 10:41:43 +0530

    code of episode 1

---
+++ App.js
@@ -0,0 +1,14 @@
+const groot = ReactDOM.createRoot(document.getElementById("groot"));
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "parent" }, "I am h1 tag"),
+    React.createElement("h2", { id: "parent" }, "I am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "parent" }, "I am h1 tag"),
+    React.createElement("h2", { id: "parent" }, "I am h2 tag"),
+  ]),
+]);
+
+setTimeout(groot.render(parent), 10000);
