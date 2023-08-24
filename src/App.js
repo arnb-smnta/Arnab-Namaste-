5f05772d3dabbad6f11fc6ce5786ea62ecf08bdf
loveumearnab2812000 <loveumearnab.2812000@gmail.com>
2023-08-24 18:10:25 +0530

    ep6 final commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Shimmer from "./components/shimmer.js";
 
 const BodyApp = () => {
   const [resrest, setresrest] = useState(resobj);
+  const [inp, setinp] = useState("");
+  const [data1, setdata1] = useState(resobj);
+
   useEffect(() => {
     fetchData();
   }, []);
@@ -20,23 +23,47 @@ const BodyApp = () => {
     const json = await data.json();
 
     console.log(json);
+
     setresrest(
-      json.data.cards[3].card.card.gridElements.infoWithStyle.restaurants
+      json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
     );
+
+    setdata1(
+      json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
+    );
+    console.log(data1);
+    console.log(resrest);
   };
 
   return resrest.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="body">
-      <h3>Search</h3>
-      <button className="searchbutton_body">Search</button>
+      <div className="search_bar">
+        <input
+          type="text"
+          value={inp}
+          onChange={(e) => {
+            setinp(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            const filteredlist = resrest.filter((res) =>
+              res.info.name.toLowerCase().includes(inp.toLowerCase())
+            );
+            setdata1(filteredlist);
+          }}
+        >
+          Search
+        </button>
+      </div>
       <div className="filter">
         <button
           onClick={() => {
-            const filterdlist = resobj.filter((res) => res.info.avgRating > 4);
+            const filterdlist = resrest.filter((res) => res.info.avgRating > 4);
             console.log(filterdlist);
-            setresrest(filterdlist);
+            setdata1(filterdlist);
           }}
           className="filter-btn"
         >
@@ -44,7 +71,7 @@ const BodyApp = () => {
         </button>
       </div>
       <div className="Res-container">
-        {resrest.map((restraunt) => {
+        {data1.map((restraunt) => {
           return (
             <RestrauntCard key={restraunt.info.id} restraunt={restraunt} />
           );
